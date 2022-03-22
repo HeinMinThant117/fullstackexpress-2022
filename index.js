57178c3b0cb3e8fb7cba5ffb432cc2f4bb624859
Jim <heinminthant1999@gmail.com>
2022-03-22 12:50:25 +0630

    Finsihed part 3a

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const { response } = require('express')
 const express = require('express')
+const morgan = require('morgan')
+const mogran = require('morgan')
 const app = express()
 
 app.use(express.json())
+app.use(
+  morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'),
+      '-',
+      tokens['response-time'](req, res),
+      'ms',
+	  JSON.stringify(req.body)
+    ].join(' ')
+  })
+)
 
 let persons = [
   {
